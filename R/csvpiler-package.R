72e1d9e02f848431b8629a95a93aec1bc0f9b7cf
Fanny Ollivier <fannyollivier1@gmail.com>
2026-03-09 22:52:33 +0100

    ajout doc package (?csvpiler)

---
+++ R/csvpiler-package.R
@@ -0,0 +1,12 @@
+#' csvpiler: Combine csv files from a folder
+#'
+#' A lightweight package to combine multiple csv files from a folder into a single csv file.
+#'
+#' Functions:
+#' \itemize{
+#'  \item\code{pile_csv_min} : Combine csv files into one csv file, no extra column
+#'  \item\code{pile_csv()}: Combine csv files into one csv file, and add 2 columns - ID (1st column) and source (last column)
+#'  }
+#'
+"_PACKAGE"
+
